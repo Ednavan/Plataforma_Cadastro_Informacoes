ca190b116426852fea72efecfb9d54f3ba8d1594
Ednavan <ednavanfisico@gmail.com>
2021-10-06 10:46:26 -0300

    post dos dados na listagem de usuarios

--- scripts/controllers/main.js
+++ scripts/controllers/main.js
@@ -1,3 +1,22 @@
-angular.module("aplicacaoRun", []).controller("controleUsuarios", function ($scope){
+angular.module("aplicacaoRun", []).controller("controleUsuarios", function ($scope, $http){
     $scope.titulo="Angularjs sendo executado";
+   
+    $scope.data = {};
+    $scope.enviodedados = function(){
+    $http({
+        url:'http://localhost:3000/listagemUsuarios',
+        method: 'POST',
+        data: $scope.data,
+        'Content-Type': 'application/json'
+    }).then(function(){
+
+   
+    })
+}
+    $http.get('http://localhost:3000/listagemUsuarios').
+    then(function(res){
+        (console.log(res))
+        $scope.listagemUsuarios = res.data
+    })
+
 });
